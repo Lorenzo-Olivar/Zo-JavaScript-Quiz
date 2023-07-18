4a7dbf44f794d2e5e77e27e6e1f124778384d375
Lorenzo-Olivar <131014446+Lorenzo-Olivar@users.noreply.github.com>
2023-07-17 19:49:04 -0700

    added answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,71 @@ var countdown = function() {
 };
 countdown();
 
-// 
+// Quiz questions
+var questionsList = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: [
+            {text: "Strings", correct: false},
+            {text: "Booleans", correct: false},
+            {text: "Alerts", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: [
+            {text: "Strings", correct: false},
+            {text: "Booleans", correct: false},
+            {text: "Alerts", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: [
+            {text: "Strings", correct: false},
+            {text: "Booleans", correct: false},
+            {text: "Alerts", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: [
+            {text: "Strings", correct: false},
+            {text: "Booleans", correct: false},
+            {text: "Alerts", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+];
+
+var questionEl = document.getElementById("question");
+var answerButton = document.getElementById("answer-btn");
+var nextButton = document.getElementById("nextbutton");
+
+var currentQuestionIndex = 0;
+var score = 0;
+
+var startQuiz = function () {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.textContent = "Next";
+    showquestion();
+};
+
+var showquestion = function () {
+    currentQuestion = questionsList[currentQuestionIndex];
+    questionEl.textContent = currentQuestion.question;
+    
+    currentQuestion.answer.forEach(answerItem => {
+        var newButton = document.createElement("button");
+        newButton.textContent = answerItem.text;
+        newButton.setAttribute('class', "btn");
+        answerButton.appendChild(newButton);
+    });
+
+};
+
+startQuiz();
